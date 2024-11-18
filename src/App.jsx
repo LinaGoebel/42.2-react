6d66e55809c7281b3ccd2565621d755d2d823a5b
LinaGoebel <galina283@freenet.de>
2024-11-18 15:38:10 +0100

    add homework 04

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import "./App.css";
 //lesson imports
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
-import Lesson04 from "./lessons/Leeson04/Lesson04";
+// import Lesson04 from "./lessons/Leeson04/Lesson04";
 
 // //homework imports
 // import Homework02 from "./homeworks/Homework02/Homework02";
 // import Homework03 from "./homeworks/Homework03/Homework03";
+import Homework04 from "./homeworks/Homework04/Homework04";
 
 //consultation imports
 // import Consultation02 from "./consultations/Consultation02/Consultation02";
@@ -20,7 +21,8 @@ function App() {
       {/* <Lesson03 /> */}
       {/* <Homework03 />   */}
       {/* <Consultation02 /> */}
-      <Lesson04 />
+      {/* <Lesson04 /> */}
+      <Homework04 />
     </div>
   );
 }
