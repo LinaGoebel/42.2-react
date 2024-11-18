0524ef3e96d45f787776e3d164d285e796982718
LinaGoebel <galina283@freenet.de>
2024-11-18 12:01:25 +0100

    add lesson 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+
 //lesson imports
 // import Lesson02 from "./lessons/Lesson02/Lesson02";
 // import Lesson03 from "./lessons/Lesson03/Lesson03";
+import Lesson04 from "./lessons/Leeson04/Lesson04";
 
 // //homework imports
 // import Homework02 from "./homeworks/Homework02/Homework02";
-import Homework03 from "./homeworks/Homework03/Homework03";
+// import Homework03 from "./homeworks/Homework03/Homework03";
 
 //consultation imports
 // import Consultation02 from "./consultations/Consultation02/Consultation02";
@@ -16,8 +18,9 @@ function App() {
       {/* <Lesson02 />
       <Homework02 /> */}
       {/* <Lesson03 /> */}
-      <Homework03 />  
+      {/* <Homework03 />   */}
       {/* <Consultation02 /> */}
+      <Lesson04 />
     </div>
   );
 }
